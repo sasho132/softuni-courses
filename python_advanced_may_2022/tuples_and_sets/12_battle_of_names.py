2201e5c7cf4b244357b54215259b18a5ecff8157
Aleksander Petrov <sasho1320@gmail.com>
2022-06-10 17:12:19 +0300

    Update python_advanced

--- python_advanced_may_2022/tuples_and_sets/12_battle_of_names.py
+++ python_advanced_may_2022/tuples_and_sets/12_battle_of_names.py
@@ -6,8 +6,23 @@ evens = set()
 for row in range(1, num + 1):
     name = input()
 
-    name_sum = sum([ord(x) for x in name]) / row
-    if name_sum % 2 == 0:
+    name_sum = sum(ord(x) for x in name) / row
+    if int(name_sum) % 2 == 0:
         evens.add(int(name_sum))
     else:
         odds.add(int(name_sum))
+
+odds_total = sum(odds)
+evens_total = sum(evens)
+result = ''
+
+if odds_total == evens_total:
+    result = odds.union(evens)
+
+elif odds_total > evens_total:
+    result = odds.difference(evens)
+
+elif evens_total > odds_total:
+    result = odds.symmetric_difference(evens)
+
+print(', '.join(str(x) for x in result))

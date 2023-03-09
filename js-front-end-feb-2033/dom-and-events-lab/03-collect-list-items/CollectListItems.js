ae18542b063fe751e7b21cea3a059d1a462c5a52
Aleksandar Petrov <sasho1320@gmail.com>
2023-03-09 15:52:08 +0200

    Update js-front-end folder

--- js-front-end-feb-2033/dom-and-events-lab/03-collect-list-items/CollectListItems.js
+++ js-front-end-feb-2033/dom-and-events-lab/03-collect-list-items/CollectListItems.js
@@ -1,3 +1,6 @@
 function extractText() {
-    // TODO
+    let items = document.querySelectorAll("ul#items li");
+    let textarea = document.querySelector("#result");
+
+    items.forEach((item) => textarea.value += item.textContent + "\n");
 }

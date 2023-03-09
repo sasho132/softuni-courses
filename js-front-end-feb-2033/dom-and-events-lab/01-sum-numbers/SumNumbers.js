c19477c3356ec7209e2b8bc3ee51ef3bd7c3d520
Aleksandar Petrov <sasho1320@gmail.com>
2023-03-09 15:45:17 +0200

    Update js-front-end folder

---
+++ js-front-end-feb-2033/dom-and-events-lab/01-sum-numbers/SumNumbers.js
@@ -0,0 +1,8 @@
+function calc() {
+    let numOne = document.getElementById("num1").value;
+    let numTwo = document.getElementById("num2").value;
+
+    let sum = Number(numOne) + Number(numTwo);
+
+    document.getElementById("sum").value = sum;
+}
---
+++ js-front-end-feb-2033/dom-and-events-lab/02-show-more/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const element = document.getElementById('text');
+    const more = document.getElementById('more');
+    element.style.display = 'inline';
+    more.style.display = 'none';
+}

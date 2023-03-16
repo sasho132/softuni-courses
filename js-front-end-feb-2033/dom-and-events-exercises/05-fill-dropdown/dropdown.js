c71508181e142053dfedfc876ea40a16aa739900
Aleksandar Petrov <sasho1320@gmail.com>
2023-03-16 06:57:53 +0200

    Update js-front-end folder

--- js-front-end-feb-2033/dom-and-events-exercises/05-fill-dropdown/dropdown.js
+++ js-front-end-feb-2033/dom-and-events-exercises/05-fill-dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    const itemText = document.getElementById("newItemText");
+    const itemValue = document.getElementById("newItemValue");
+    const menuSelector = document.getElementById("menu");
+    let newOption = document.createElement("option");
+    newOption.textContent = itemText.value;
+    newOption.value = itemValue.value;
+    menuSelector.appendChild(newOption);
+
+    itemText.value = "";
+    itemValue.value = "";
 }
--- js-front-end-feb-2033/dom-and-events-exercises/06-table-search-engine/solution.js
+++ js-front-end-feb-2033/dom-and-events-exercises/06-table-search-engine/solution.js
@@ -1,8 +1,21 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+    function onClick() {
+        const searchInCells = Array.from(
+            document.querySelectorAll(".container > tbody > tr > td")
+        );
+        const input = document.getElementById("searchField");
+        let searched = input.value;
+        console.log(searched);
 
-   }
+        searchInCells.map((el) => {
+            if (el.textContent.includes(searched)) {
+                const row = el.parentElement;
+                row.setAttribute("class", "select");
+            }
+        });
+
+        input.value = "";
+    }
 }

9232a2d02d1409bd71eed3350b414679309e557b
Aleksandar Petrov <sasho1320@gmail.com>
2023-03-16 10:54:32 +0200

    Update js-front-end folder

--- js-front-end-feb-2033/dom-and-events-exercises/07-format-the-text/solution.js
+++ js-front-end-feb-2033/dom-and-events-exercises/07-format-the-text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+    const output = document.getElementById("output");
+    const input = document.getElementById("input");
+    let text = Array.from(input.value.split("."));
+
+    let chunkSize = 3;
+    for (let i = 0; i < text.length; i += chunkSize) {
+        let chunk = text.slice(i, i + chunkSize).filter((el) => el !== "").join(".");
+        if (chunk.length > 0) {
+          let p = document.createElement("p");
+        chunk += ".";
+        p.textContent = chunk;
+        output.appendChild(p);
+        }      
+    }
+    input.value = "";
 }

5b6791cc12173077dbde85273cfb4bf54a2b816f
Aleksandar Petrov <sasho1320@gmail.com>
2023-03-25 13:48:29 +0200

    Update js-front-end folder

--- js-front-end-feb-2033/http-and-rest/05-messenger/app.js
+++ js-front-end-feb-2033/http-and-rest/05-messenger/app.js
@@ -1,5 +1,43 @@
 function attachEvents() {
-    // TODO:
+    const refreshBtn = document.getElementById("refresh");
+    const submitBtn = document.getElementById("submit");
+    const messageArea = document.getElementById('messages');
+    const messageAuthor = document.querySelector('#controls > div:nth-child(1) > input:nth-child(2)');
+    const messageBody = document.querySelector('#controls > div:nth-child(2) > input:nth-child(2)');
+    const BASE_URL = `http://localhost:3030/jsonstore/messenger`;
+
+    refreshBtn.addEventListener("click", getMessages);
+    submitBtn.addEventListener('click', postMessage);
+    
+    function postMessage() {
+        messageArea.value = '';
+        let author = messageAuthor.value;
+        let content = messageBody.value;
+        let httpHeaders = {
+            method: 'post',
+            body: JSON.stringify({ author, content }),
+        }
+        fetch(BASE_URL, httpHeaders)
+            .then((res) => res.json())
+            .then(messageAuthor.value = '')
+            .then(messageBody.value = '')
+            .then(getMessages)
+    }
+
+    async function getMessages() {
+        try {
+            const messagesData = await fetch(`${BASE_URL}`);
+            const messages = await messagesData.json();
+            let messageData = '';
+            Object.values(messages).forEach((message) => {
+                messageData += `${message.author}: ${message.content}\n`;
+            })
+            messageArea.value = messageData.trim();
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
 }
 
 attachEvents();
--- js-front-end-feb-2033/http-and-rest/06-phonebook/app.js
+++ js-front-end-feb-2033/http-and-rest/06-phonebook/app.js
@@ -1,5 +1,66 @@
 function attachEvents() {
-    // TODO:
+    const phoneBookList = document.getElementById("phonebook");
+    const loadBtn = document.getElementById("btnLoad");
+    const createBtn = document.getElementById("btnCreate");
+    const personInput = document.getElementById("person");
+    const phoneInput = document.getElementById("phone");
+    const BASE_URL = `http://localhost:3030/jsonstore/phonebook/`;
+
+    loadBtn.addEventListener("click", loadContacts);
+    createBtn.addEventListener("click", createContact);
+
+    async function loadContacts() {
+        try {
+            const phoneBookData = await fetch(BASE_URL);
+            const phoneBookContacts = await phoneBookData.json();
+            phoneBookList.innerHTML = '';
+            Object.values(phoneBookContacts).forEach((el) => {
+                let person = el.person;
+                let phone = el.phone;
+                let li = document.createElement("li");
+                let deleteBtn = document.createElement("button");
+                deleteBtn.textContent = "Delete";
+                deleteBtn.id = el._id;
+                deleteBtn.addEventListener("click", deleteFromPhoneBook);
+                li.textContent = `${person}: ${phone}`;
+                li.appendChild(deleteBtn);
+                phoneBookList.appendChild(li);
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    function createContact() {
+        let person = personInput.value;
+        let phone = phoneInput.value;
+        let htmlHeaders = {
+            method: "post",
+            body: JSON.stringify({ person, phone }),
+        };
+        fetch(BASE_URL, htmlHeaders)
+            .then((res) => res.json())
+            .then(personInput.value = "")
+            .then(phoneInput.value = "")
+            .then(loadContacts())
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    function deleteFromPhoneBook() {
+        const id = this.id;
+        const httpHeaders = {
+            method: "delete"
+        };
+
+        fetch(`${BASE_URL}${id}`, httpHeaders)
+            .then((res) => res.json())
+            .then(loadContacts())
+            .catch((err) => {
+                console.log(err);
+            })
+    }
 }
 
 attachEvents();
